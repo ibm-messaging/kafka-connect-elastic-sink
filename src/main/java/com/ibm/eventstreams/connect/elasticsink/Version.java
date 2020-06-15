db9b3a90d6cdd8e567555d3673d03d2a99a02198
Andrew Schofield <andrew_schofield@uk.ibm.com>
2020-06-15 11:46:29 +0100

    Initial release.
    Signed-off-by: Andrew Schofield <andrew_schofield@uk.ibm.com>

--- src/main/java/com/ibm/eventstreams/connect/elasticsink/Version.java
+++ src/main/java/com/ibm/eventstreams/connect/elasticsink/Version.java
@@ -16,5 +16,5 @@
 package com.ibm.eventstreams.connect.elasticsink;
 
 public class Version {
-    public static final String VERSION = "0.0.4-SNAPSHOT"; // Must match the value in pom.xml
+    public static final String VERSION = "1.0.0"; // Must match the value in pom.xml
 }
